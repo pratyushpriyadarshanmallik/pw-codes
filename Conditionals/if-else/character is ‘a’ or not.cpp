f69345b3ea742042b483d1bc7c59f64db8f3f8bd
pratyushpriyadarshanmallik <93968652+pratyushpriyadarshanmallik@users.noreply.github.com>
2022-11-12 06:46:02 +0530

    Create character is ‘a’ or not.cpp

---
+++ Conditionals/if-else/character is ‘a’ or not.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    char ch;
+    cin>>ch;
+    if(ch=='a'){
+        cout<<"character is'a'";
+    }else{
+        cout<<"character is not 'a'";
+    }
+    return 0;
+}
