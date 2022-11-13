e842b09686c91f050bac92b6cd3ed2578d52c516
pratyushpriyadarshanmallik <93968652+pratyushpriyadarshanmallik@users.noreply.github.com>
2022-11-13 20:17:10 +0530

    Create sum-of-first-10-natural-no.s.cpp

---
+++ Loops/do-while/sum-of-first-10-natural-no.s.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int i=1;
+    int sum=0;
+    do{
+        sum+=i;
+        i++;
+    }while(i<=10
+    );
+    cout<<sum;
+    return 0;
+}
