8f22544a759dde9d08f4729c758993128a9d8d01
pratyushpriyadarshanmallik <93968652+pratyushpriyadarshanmallik@users.noreply.github.com>
2022-11-13 15:42:05 +0530

    Create sum-of-first-10-natural-no.s.cpp

---
+++ Loops/for-loop/sum-of-first-10-natural-no.s.cpp
@@ -0,0 +1,11 @@
+// Print the sum of the first 10 natural numbers.
+#include<iostream>
+using namespace std;
+int main(){
+    int sum=0;
+    for(int i=1;i<=10;i++){
+        sum+=i;
+    }
+    cout<<sum;
+    return 0;
+}
