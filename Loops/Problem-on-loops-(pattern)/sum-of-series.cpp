6c9011b6f4c73bd5d6016fbff48b0980968eec17
pratyushpriyadarshanmallik <93968652+pratyushpriyadarshanmallik@users.noreply.github.com>
2022-11-16 15:26:51 +0000

    Create sum-of-series.cpp

---
+++ Loops/Problem-on-loops-(pattern)/sum-of-series.cpp
@@ -0,0 +1,19 @@
+/*Find the sum of the following series  
+1 - 2 + 3 - 4 ... n */  
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    int ans=0;
+    for(int i=1;i<=n;i++){
+        if(i%2==0){
+            ans-=i;
+        }
+        else{
+            ans+=i;
+        }
+    }
+    cout<<ans;
+    return 0;
+}
