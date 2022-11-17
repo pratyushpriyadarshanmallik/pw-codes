e2350811f3f2995d08b2b8f72a565d39d7d8f7cc
pratyushpriyadarshanmallik <93968652+pratyushpriyadarshanmallik@users.noreply.github.com>
2022-11-17 14:41:30 +0000

    Create decimal-to-binary.cpp

---
+++ Number System/decimal-to-binary.cpp
@@ -0,0 +1,17 @@
+//Decimal to Binary
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;//5
+    int ans=0;
+    int power=1;
+    while(n>0){
+        int paritydigit =n%2;
+        ans+=(paritydigit*power);
+        power*=10;
+        n/=2;
+    }
+    cout<<ans<<endl;
+    return 0;
+}
