2b7783da404a281f9210b64f4c07c482cb9c789e
pratyushpriyadarshanmallik <93968652+pratyushpriyadarshanmallik@users.noreply.github.com>
2022-11-17 15:35:48 +0530

    Create a-to-the-power-b.cpp

---
+++ Loops/Problem on loops(number)/a-to-the-power-b.cpp
@@ -0,0 +1,14 @@
+//Given 2 numbers a and b Find a raise to the power b
+
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b;
+    cin>>a>>b;
+    int ans=1;
+    for(int i=1;i<=b;i++){
+        ans*=a;
+    }
+    cout<<ans;
+    return 0;
+}
