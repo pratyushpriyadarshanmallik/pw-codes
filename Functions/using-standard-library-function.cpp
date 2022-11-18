56cafa3752c4e9bb6567115df6fe0d550474ce34
pratyushpriyadarshanmallik <93968652+pratyushpriyadarshanmallik@users.noreply.github.com>
2022-11-18 14:43:13 +0000

    Create using-standard-library-function.cpp

---
+++ Functions/using-standard-library-function.cpp
@@ -0,0 +1,11 @@
+//USING C++ Standard library functions
+#include<iostream>
+#include<cmath>
+using namespace std;
+int main(){
+    int a,b,n;
+    cin>>a>>b>>n;
+    cout<<a<<" to the power "<<b<<" is: "<<pow(a,b)<<endl;
+    cout<<"Square root of "<<n<<" is: "<<sqrt(n)<<endl;
+    return 0;
+}
