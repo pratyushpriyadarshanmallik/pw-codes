75349747529b5c35a85dd9318a0e9493e3ab899a
pratyushpriyadarshanmallik <93968652+pratyushpriyadarshanmallik@users.noreply.github.com>
2022-11-22 14:05:40 +0000

    Create max-element-in-array.cpp

---
+++ Array/max-element-in-array.cpp
@@ -0,0 +1,20 @@
+//Find the maximum value out of all the elements in the array.
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    int arr[n];
+    int max=arr[0];
+    int size=sizeof(arr)/sizeof(arr[0]);
+    for(int i=0;i<size;i++){
+        cin>>arr[i];
+    }
+    for(int i=1;i<size;i++){
+        if(arr[i]>max){
+            max=arr[i];
+        }
+    }
+    cout<<max<<endl;
+    return 0;
+}
