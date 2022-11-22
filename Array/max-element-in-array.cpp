cd12ca2690fa30fdec60379bf7bb4f5181d6c0fc
pratyushpriyadarshanmallik <93968652+pratyushpriyadarshanmallik@users.noreply.github.com>
2022-11-22 14:07:25 +0000

    Update max-element-in-array.cpp

--- Array/max-element-in-array.cpp
+++ Array/max-element-in-array.cpp
@@ -5,11 +5,11 @@ int main(){
     int n;
     cin>>n;
     int arr[n];
-    int max=arr[0];
     int size=sizeof(arr)/sizeof(arr[0]);
     for(int i=0;i<size;i++){
         cin>>arr[i];
     }
+    int max=arr[0];
     for(int i=1;i<size;i++){
         if(arr[i]>max){
             max=arr[i];
