f3c487171afa45a0287dcb8677328ed8fe997386
pratyushpriyadarshanmallik <93968652+pratyushpriyadarshanmallik@users.noreply.github.com>
2022-12-27 08:58:42 +0000

    Create cube-of-number.cpp

---
+++ Assignment-Solutions/Assignment-1(Variables)/cube-of-number.cpp
@@ -0,0 +1,11 @@
+//Write a C++ program to calculate the cube of a number.
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter number: ";
+    cin>>n;
+    int ans =n*n*n;
+    cout<<"Cube of number: "<<ans<<endl;
+    return 0;
+}
