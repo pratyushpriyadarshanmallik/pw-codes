235df0e312337405b4576d7210f8c6ffa0119212
pratyushpriyadarshanmallik <93968652+pratyushpriyadarshanmallik@users.noreply.github.com>
2022-12-27 08:19:12 +0000

    Create ASCII-number.cpp

---
+++ Assignment-Solutions/Assignment-1(Variables)/ASCII-number.cpp
@@ -0,0 +1,11 @@
+//Print the ASCII value of character ‘U’
+#include<iostream>
+using namespace std;
+int main(){
+    char ch;
+    cout<<"Enter Character: ";
+    cin>>ch;
+    cout<<"ASCII number: "<<(int) ch;
+
+    return 0;
+}
