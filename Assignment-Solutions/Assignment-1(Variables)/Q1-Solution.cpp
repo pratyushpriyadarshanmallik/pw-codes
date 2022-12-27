e03c85e891fc8718431bda5d3e48c95eb86f7a11
pratyushpriyadarshanmallik <93968652+pratyushpriyadarshanmallik@users.noreply.github.com>
2022-12-27 08:09:53 +0000

    Create Q1-Solution.cpp

---
+++ Assignment-Solutions/Assignment-1(Variables)/Q1-Solution.cpp
@@ -0,0 +1,15 @@
+//Take 2 integer values in two variables x and y and print their product.
+#include<iostream>
+using namespace std;
+int main(){
+    int x;
+    cout<<"Enter x: ";
+    cin>>x;
+    int y;
+    cout<<"Enter y: ";
+    cin>>y;
+    int ans =x*y;
+    cout<<"x * y = "<<ans;
+    return 0;
+
+}
