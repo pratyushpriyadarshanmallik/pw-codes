ffb96b2ec5ecd73c06d53fc82e959096ce228d4a
pratyushpriyadarshanmallik <93968652+pratyushpriyadarshanmallik@users.noreply.github.com>
2022-12-28 06:52:34 +0000

    Create same-or-not.cpp

---
+++ Assignment-Solutions/Assignment-2(Operators)/same-or-not.cpp
@@ -0,0 +1,13 @@
+//Write a program to check whether two numbers (entered by user) are equal or not.
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b;
+    cin>>a>>b;
+    if(a==b){
+        cout<<"1"<<endl;
+    }else{
+        cout<<"0"<<endl;
+    }
+    return 0;
+}
