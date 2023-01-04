5c28d20a89bfc7e8de227229b0881a8f5daba6f9
pratyushpriyadarshanmallik <93968652+pratyushpriyadarshanmallik@users.noreply.github.com>
2023-01-04 19:03:45 +0530

    Create sum-of-n-natural-numbers.cpp

---
+++ Assignment-Solutions/Assignment-4(Problem-on-Loops)/sum-of-n-natural-numbers.cpp
@@ -0,0 +1,14 @@
+/*Write a program to find the sum of n natural numbers*/
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter a number: ";
+    cin>>n;
+    int sum=0;
+    for(int i=1;i<=n;i++){
+        sum+=i;
+    }
+    cout<<sum<<endl;
+    return 0;
+}
