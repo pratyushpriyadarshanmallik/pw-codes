19e7c0648c1e590ec6255e4bf5c12a53d4857160
pratyushpriyadarshanmallik <93968652+pratyushpriyadarshanmallik@users.noreply.github.com>
2023-01-13 15:38:37 +0000

    Create odd-between-a-and-b.cpp

---
+++ Assignment-Solutions/Assignment-5(Functions)/odd-between-a-and-b.cpp
@@ -0,0 +1,18 @@
+/*Given two numbers a and b, write a program using functions to print all the odd numbers
+between them.*/
+#include<iostream>
+using namespace std;
+void odd(int a,int b){
+    for(int i=a;i<b;i++){
+        if(i%2!=0){
+            cout<<i<<" ";
+        }
+    }
+    return;
+}
+int main(){
+    int a,b;
+    cin>>a>>b;
+    odd(a,b);
+    cout<<"\n";
+}
