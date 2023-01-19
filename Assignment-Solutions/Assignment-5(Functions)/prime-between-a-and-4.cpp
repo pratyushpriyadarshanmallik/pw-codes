99eddd0f5bf09c16020011652b9505e278829bcd
pratyushpriyadarshanmallik <93968652+pratyushpriyadarshanmallik@users.noreply.github.com>
2023-01-19 14:12:32 +0000

    Update prime-between-a-and-4.cpp

--- Assignment-Solutions/Assignment-5(Functions)/prime-between-a-and-4.cpp
+++ Assignment-Solutions/Assignment-5(Functions)/prime-between-a-and-4.cpp
@@ -7,12 +7,9 @@ bool isprime(int n){
     for(int i=2;i<n;i++){
         if(n%i==0){
             return false;
-            continue;
-        }
-        else{
-            return true;
         }
     }
+    return true;
 }
 int main(){
     int a,b;
