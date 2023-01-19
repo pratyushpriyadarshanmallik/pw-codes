c78b64fa0b479eba4f397bba4fdfe92dfc97c968
pratyushpriyadarshanmallik <93968652+pratyushpriyadarshanmallik@users.noreply.github.com>
2023-01-19 19:32:30 +0530

    Create prime-between-a-and-4.cpp

---
+++ Assignment-Solutions/Assignment-5(Functions)/prime-between-a-and-4.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+bool isprime(int n){
+    if(n==2){
+        return true;
+    }
+    for(int i=2;i<n;i++){
+        if(n%i==0){
+            return false;
+            continue;
+        }
+        else{
+            return true;
+        }
+    }
+}
+int main(){
+    int a,b;
+    cin>>a>>b;
+    for(int i=a;i<b;i++){
+        if(isprime(i)==true){
+            cout<<i<<" ";
+        }
+    }
+    return 0;
+}
