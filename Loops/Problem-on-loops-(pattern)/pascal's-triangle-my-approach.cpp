dc4e3d6183305d52b0217ebf8e44a4033e3c8ead
pratyushpriyadarshanmallik <93968652+pratyushpriyadarshanmallik@users.noreply.github.com>
2023-02-03 20:09:37 +0530

    Create pascal's-triangle-my-approach.cpp

---
+++ Loops/Problem-on-loops-(pattern)/pascal's-triangle-my-approach.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int fact(int n){
+  int ans=1;
+  for(int i=1;i<=n;i++){
+    ans*=i;
+  }
+  return ans;
+}
+int main(){
+  int n;
+  cin>>n;
+  for(int i=0;i<n;i++){
+    for(int j=0;j<=i;j++){
+      cout<<fact(i)/(fact(j)*fact(i-j))<<" ";
+    }
+    cout<<endl;
+  }
+  return 0;
+}
