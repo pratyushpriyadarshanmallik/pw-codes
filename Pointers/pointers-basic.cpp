bd22123ac71725558f76ee7f328f8a9e853dce4d
pratyushpriyadarshanmallik <93968652+pratyushpriyadarshanmallik@users.noreply.github.com>
2023-03-14 21:23:10 +0530

    Update pointers-basic.cpp

--- Pointers/pointers-basic.cpp
+++ Pointers/pointers-basic.cpp
@@ -15,5 +15,22 @@ int main(){
     cout<<pointer<<" ";
     pointer = &x; //Re assigning the pointer 
     cout<<pointer<<" ";
+    //Dereferencing - operator
+    int z =10;
+    int *ptr3 = &z;
+    cout<<"Address of the pointer: "<<ptr3<<endl;
+    cout<<"Value inside pointer:"<<*ptr3<<endl;
+    z = 23;
+    cout<<"Address of the pointer: "<<ptr3<<endl;
+    cout<<"Value inside pointer after dereferencing:"<<*ptr3<<endl;
+
+    //Updating value with pointer
+    *ptr3=50;
+    cout<<"New value of z: "<<z<<endl;
+    cout<<"New value pointed by ptr3: "<<*ptr3<<endl;
+
+    //Storing the value of a pointer inside another variable
+    int val=*ptr3;
+    cout<<"Val: "<<val<<endl;
     return 0;
 }
